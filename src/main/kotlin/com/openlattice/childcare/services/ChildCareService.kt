37e6b12a531ba9ab31176780a6a7a153ac68f631
Katherine Bernstein <katherine@openlattice.com>
2020-04-16 00:32:23 -0700

    rate limit

--- src/main/kotlin/com/openlattice/childcare/services/ChildCareService.kt
+++ src/main/kotlin/com/openlattice/childcare/services/ChildCareService.kt
@@ -1,6 +1,7 @@
 package com.openlattice.childcare.services
 
 import com.auth0.exception.Auth0Exception
+import com.google.common.util.concurrent.RateLimiter
 import com.openlattice.childcare.configuration.ChildCareConfiguration
 import com.openlattice.shuttle.MissionControl
 import org.slf4j.LoggerFactory
@@ -12,6 +13,8 @@ class ChildCareService(
 ) {
 
     lateinit var token: String
+    private val rateLimiter = RateLimiter.create(1_000.0)
+
 
     companion object {
         protected val logger = LoggerFactory.getLogger(ChildCareService::class.java)
@@ -31,6 +34,7 @@ class ChildCareService(
     }
 
     fun getJWTToken(): String {
+        rateLimiter.acquire()
         return token
     }
 
