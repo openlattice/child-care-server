0b852dbffc744f9b4385a8bdc24cf37ee5eeb04b
Drew <drew@openlattice.com>
2020-11-20 16:45:13 -0500

    Use google for caching

--- src/main/kotlin/com/openlattice/childcare/services/ChildCareService.kt
+++ src/main/kotlin/com/openlattice/childcare/services/ChildCareService.kt
@@ -1,47 +1,37 @@
 package com.openlattice.childcare.services
 
-import com.auth0.exception.Auth0Exception
+import com.google.common.base.Supplier
+import com.google.common.base.Suppliers
 import com.google.common.util.concurrent.RateLimiter
 import com.openlattice.auth0.Auth0Delegate;
 import com.openlattice.authentication.Auth0Configuration;
 import com.openlattice.childcare.configuration.ChildCareConfiguration
 import org.slf4j.LoggerFactory
-import org.springframework.scheduling.annotation.Scheduled
-
+import java.util.concurrent.TimeUnit
 
 class ChildCareService(
         val config: ChildCareConfiguration,
-        val auth0Configuration: Auth0Configuration
+        auth0Configuration: Auth0Configuration
 ) {
+    private val auth0Client: Auth0Delegate = Auth0Delegate.fromConfig(auth0Configuration)
 
-    lateinit var token: String
+    private val tokenSupplier: Supplier<String> = Suppliers.memoizeWithExpiration({
+        try {
+            auth0Client.getIdToken(config.username, config.password)
+        } catch (ex: Exception) {
+            logger.error("Failed to retrieve a token from auth0", ex)
+            throw ex
+        }
+    }, 10, TimeUnit.HOURS)
 
-    private val auth0Client: Auth0Delegate
+    companion object {
+        private val logger = LoggerFactory.getLogger(ChildCareService::class.java)
+    }
 
     private val rateLimiter = RateLimiter.create(1_000.0)
 
-
-    companion object {
-        protected val logger = LoggerFactory.getLogger(ChildCareService::class.java)
-    }
-
-    init {
-        this.auth0Client = Auth0Delegate.fromConfig(auth0Configuration)
-        refresh()
-    }
-
-    @Scheduled(fixedRate = 60000 * 60 * 10) // 10 hours
-    private fun refresh() {
-        try {
-            token = auth0Client.getIdToken(config.username, config.password)
-        } catch (e: Auth0Exception) {
-            logger.error("Unable to reload jwt token: ", e)
-        }
-    }
-
     fun getJWTToken(): String {
         rateLimiter.acquire()
-        return token
+        return tokenSupplier.get()
     }
-
 }
